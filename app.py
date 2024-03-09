618c382dc4792771f30b1f5d9e729c727ec6d9d7
jorge <jorgezafra94@hotmail.com>
2024-03-09 14:38:59 -0600

    changing logic and implementing dictionaries instead of lists for stores and items

--- app.py
+++ app.py
@@ -1,61 +1,58 @@
+import uuid
+
 from flask import Flask, request
 
-app = Flask(__name__)
+from db import stores, items
 
-stores = [
-    {
-        "name": "My store",
-        "items": [
-            {
-                "name": "chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
+app = Flask(__name__)
 
 
 @app.get("/stores")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/stores")
 def add_store():
-    data = request.get_json()
-    if "name" not in data.keys():
+    store_data = request.get_json()
+    if "name" not in store_data.keys():
         raise ValueError("Store don't have a name")
-    new_store = {"name": data.get("name"), "items": data.get("items", [])}
-    stores.append(new_store)
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
     return new_store, 201
 
 
-@app.get("/stores/<store_name>")
-def get_store(store_name):
-
-    for store in stores:
-        if store.get("name") == store_name:
-            return store
-    return "{message: Store not found}", 404
-
-
-@app.get("/stores/<store_name>/items")
-def get_store_items(store_name):
-    for store in stores:
-        if store.get("name") == store_name:
-            return store.get("items")
-    return "{message: Store not found}", 404
+@app.get("/stores/<store_id>")
+def get_store(store_id):
+    store = stores.get(store_id, None)
+    if not store:
+        return "{message: Store not found}", 404
+    return store
 
 
-@app.post("/stores/<store_name>/items")
-def add_item_to_store(store_name):
-    for store in stores:
-        if store_name == store.get("name"):
-            data = request.get_json()
-            new_item = {"name": data.get("name"), "price": float(data.get("price"))}
-            store["items"].append(new_item)
-            return new_item, 201
-    return "{message: store not found}", 404
+@app.get("/items")
+def get_items():
+    return {"items": list(items.values())}
+
+
+@app.post("/items")
+def add_item():
+    item_data = request.get_json()
+    if item_data.get("store_id") not in stores:
+        return "{message: store not found}", 404
+    item_id = uuid.uuid4().hex
+    new_item = {**item_data, "id": item_id}
+    items[item_id] = new_item
+    return new_item, 201
+
+
+@app.get("/items/<item_id>")
+def get_item(item_id):
+    item = items.get(item_id, None)
+    if not item:
+        return "{message: Store not found}", 404
+    return item
 
 
 if __name__ == "__main__":
